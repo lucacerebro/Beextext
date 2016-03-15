cc8e6ab816dfa7e3fec52a9aff1dab1f4b645460
testbeexel1 <testbeexel@gmail.com>
2016-03-15 16:39:39 +0100

    seconda modifica beexeltest
    modifica

--- Calculator-FromGithub/src/Calculator.php
+++ Calculator-FromGithub/src/Calculator.php
@@ -8,7 +8,7 @@
   * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
  *
- * 44545
+ * 
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
